d6bd3f0fbb24f266d90af5136bdd22c51c329db2
BuildTools <33381901+JustinLex@users.noreply.github.com>
2018-02-27 16:13:47 +0100

    begin implementing UBX protocol

--- src/mipslabwork.c
+++ src/mipslabwork.c
@@ -26,7 +26,17 @@ char textstring[] = "text, more text, and even more text!";
 /* Interrupt Service Routine */
 void user_isr( void )
 {
-  if(IFS(0) & 0x800) { //INT2
+  /*if uart interrupt, call uart packet handler
+
+
+  if(IFS(0) & 0x100) { //T2
+    //see what page we're on, then write the correct data to the screen and update
+    display_update();
+    IFSCLR(0) = 0x100;
+  }
+
+  //button interrupts for changing pages?
+  /*if(IFS(0) & 0x800) { //INT2
     tick( &mytime );
     tick( &mytime );
     tick( &mytime );
@@ -35,19 +45,7 @@ void user_isr( void )
     display_update();
     IFSCLR(0) = 0x800;
 
-  }
-  
-  if(IFS(0) & 0x100) { //T2
-    timeoutcount++;
-    if(timeoutcount >= 10) {
-      tick( &mytime );
-      timeoutcount = 0;
-    }
-    time2string( textstring, mytime );
-    display_string( 3, textstring );
-    display_update();
-    IFSCLR(0) = 0x100;
-  }
+  }*/
 
   return;
 }
@@ -55,20 +53,23 @@ void user_isr( void )
 /* Lab-specific initialization goes here */
 void labinit( void ) {
 
-  /*initialize timer2*/
+
+
+  /*initialize timer2*/ //TODO: Reconfig for project
   T2CONCLR = 0xFFFF; //disable timer 2 and clear registers if enabled
   T2CONSET = 0x70; //set timer prescale to 256:1 (we need to count to 8M cycles, which is not possible with 1:64 or lower)
   TMR2 = 0x0; //clear timer 2 count
   PR2 = 0x7a12; //set period to 31250 = 100ms@312.5khz (80Mhz@1:256)
   IFSCLR(0) = 0x100; //reset timer 2 Interrupt flag
-  IPC(2) = 0x1F; //set timer2 interrupts to highest priority
+  IPC(2) = 0x1A; //set timer2 interrupts to 2nd highest priority
   IEC(0) = 0x100; //enable timer 2 interrupts
   T2CONSET = 0x8000; //enable timer
 
-  /*initialize External Interrupt 2*/
-  IFSCLR(0) = 0x800; //clear INT2IF
-  IPCSET(2)= 0x1F000000; //set INT2 highest priority
-  IECSET(0) = 0x800; //enable External Interrupt 2 (INT2)
+  /*initialize uart2 interrupt*/
+  IFSCLR(1) = 0x200; //reset UART2 Interrupt flag
+  IPC(8) = 0x1F; //set UART2 interrupts to highest priority
+  IEC(0) = 0x100; //enable UART2 interrupts //TODO: (CANT FIND IN 320 SERIES REGISTER??)
+  setupuart(); //initialize uart2 port
 
   enable_interrupt();
   return;
@@ -76,10 +77,20 @@ void labinit( void ) {
 
 /* This function is called repetitively from the main program */
 void labwork( void ) {
-  prime = nextprime(prime);
-  display_string(0, itoaconv(prime));
-  //int flagged = IFS(0) & 0x0100;
-  //display_debug(&flagged);
-  display_update();
+  //format packets into ready-to-print strings and images
+  //constantly handle button presses for screen change? or button interrupts?
   return;
+}
+
+//set up uart bus and start listening to the gps module
+void setupuart() {
+  /*See PIC32MX3XX datasheet for addresses, Reference manual 21 for settings.*/
+  U2BRG = 129; // set uart baud to 38400 baud when BRGH=0 [=(80Mhz/16/38400)-1]
+  U2STA = 0x1000; //00 0 1 0 0 0 0 00 0 0 0 0 0 0 set only RX, interrupt when buffer has data
+  U2MODE = 0x8000; //1 0 0 0 0 0 00 0 0 0 0 0 00 0 Enable UART2, no special options
+}
+
+//disable uart bus so we can update the display
+void disableuart() {
+  U2MODE = 0;
 }
--- src/packet.c
+++ src/packet.c
@@ -1,1 +1,29 @@
 //functions for processing packet data
+
+_Bool in_packet = 0; //0: no μ found, 1: μ found, no b yet
+
+_Bool in_checksum = 0;
+
+uint8_t header_bits_read = 0;
+
+uint16_t packet_type = 0;
+
+uint16_t packet_length = 0;
+
+uint8_t* payloadptr;
+
+
+void handlepacket(void) {
+
+  //scan for μb packet start
+  if(!in_packet) {
+    if(U2RXREG == 'μ')
+    in_packet = 1;
+  }
+  if(!in_packet) {
+    if(U2RXREG == 'b')
+      in_packet = 2;
+    else in_packet = 0;
+  }
+
+}

a434c85c9ee0e67a1cb213498aa0e8902153c620
BuildTools <33381901+JustinLex@users.noreply.github.com>
2018-03-04 15:10:49 +0100

    change baudrate back to 38400

--- src/uart.c
+++ src/uart.c
@@ -7,7 +7,7 @@
 //set up uart bus and start listening to the gps module
 void uart_start_rx() {
   /*See PIC32MX3XX datasheet for addresses, Reference manual 21 for settings.*/
-  U2BRG = 520; // set uart baud to 38400 baud when BRGH=0 [=(80Mhz/16/38400)-1] 520 129
+  U2BRG = 129; // set uart baud to 38400 baud when BRGH=0 [=(80Mhz/16/38400)-1] 520 129
   U2STA = 0x1080; //00 0 1 0 0 0 0 10 0 0 0 0 0 0 set only RX, interrupt whenever buffer is 3/4 full
   U2MODE = 0x8000; //1 0 0 0 0 0 00 0 0 0 0 0 00 0 Enable UART2, no special options
 }

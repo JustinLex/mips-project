bfdd3fbcd834989e44ef819ae4e9e4164e45e6cf
Freshfish <gefrish@gmail.com>
2018-03-05 00:06:51 +0100

    Added incomplete clock and changed function inputs

--- src/compass.c
+++ src/compass.c
@@ -2,6 +2,77 @@
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include <math.h> /*Declarations of sin,cos and so on*/
 
+const _Bool clock_min_sec [32][32]={ //clock hour hand
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+
+}
+
+const _Bool clock_min_sec [32][32]={ //clock min/sec hand
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+
+};
 const _Bool north [32][32]={ //original north facing matrix on which we will perform rotation
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -36,23 +107,26 @@ const _Bool north [32][32]={ //original north facing matrix on which we will per
   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
   };
 
-_Bool compass [32][32]; //rotated matrix
-uint8_t compass_data[128]; //rotated matrix in a display_image() compatible array
-int rot=0; //variable for the humorous rotating triangle page
+_Bool compass [32][32]; //rotated compass matrix
+_Bool clock_sec_array [32][32];
+_Bool clock_min_array [32][32];
+_Bool clock_hour_array [32][32]; //rotated time matrices
+_Bool clocktotal [32][32]; //final time matrix
+uint8_t compass_data[128]; //rotated compass matrix in a display_image() compatible array
+uint8_t clock_data[128]; //rotated time matrix in a display_image() compatible array
 
-
-void clear_matrix(void) //clean current compass matrix
+void clear_matrix(_Bool array[32][32]) //clean current compass matrix
 {
   int i,j;
   for(i=0; i<32; i++)
     for(j=0; j<32; j++)
     {
-       compass[i][j]=0;
+       array[i][j]=0;
     }
 
 }
 
-void rotation(double radian)   // function for rotating the north matrix about the middle/origin for X radians
+void rotation(double radian, _Bool array[32][32], _Bool orig_arrayx[][])   // function for rotating the north matrix about the middle/origin for X radians
 {                              // and storing it in compass matrix
   double x,y;
   double rx,ry; //rotated x and y
@@ -61,7 +135,7 @@ void rotation(double radian)   // function for rotating the north matrix about t
   for(i=0; i<32; i++)
     for(j=0; j<32; j++)
     {
-      if(north[i][j]==0) //skip 0 values since we care only about 1s
+      if(clock[i][j]==0) //skip 0 values since we care only about 1s
       continue;
       x=(double) -15 + j; //convert i into x coordinates (origin=[15][15])
       y=(double) 15 - i; //convert j into y coordinates
@@ -71,51 +145,90 @@ void rotation(double radian)   // function for rotating the north matrix about t
       ry=(int)(ry < 0 ? (ry - 0.5) : (ry + 0.5)); //rounding double to int
       rj=15+rx;
       ri=15-ry; //getting rotated matrix coordinates
-      compass[ri][rj]=1; //setting the rotated bit in the final matrix to 1
+      array[ri][rj]=1; //setting the rotated bit in the final matrix to 1
     }
   }
 
-void fix_matrix(void){ //deletes bit gaps
+void fix_matrix(_Bool array[32][32]){ //deletes bit gaps
   int i,j,k;
   for(i=0; i<32; i++){
     for(j=0; j<32; j++)
     {
-      if(compass[i-1][j]&&compass[i+1][j]||compass[i][j-1]&&compass[i][j+1]) //checking if there are any gaps
-        compass[i][j]=1;                                                     //and filling them with 1s
-      if(compass[i-1][j]&&compass[i+2][j]||compass[i-2][j]&&compass[i+1][j]) //check for wider vertical gaps
-        { compass[i][j]=1;
-          compass[i+1][j]=1;
-          compass[i-1][j]=1;}
-      if(compass[i][j+1]&&compass[i][j-2]||compass[i][j-1]&&compass[i][j+2]) //check for wider horizontal gaps
-        { compass[i][j]=1;
-          compass[i][j+1]=1;
-          compass[i][j-1]=1;}
+      if(array[i-1][j]&&array[i+1][j]||array[i][j-1]&&array[i][j+1]) //checking if there are any gaps
+        array[i][j]=1;                                                     //and filling them with 1s
+      if(array[i-1][j]&&array[i+2][j]||array[i-2][j]&&array[i+1][j]) //check for wider vertical gaps
+        { array[i][j]=1;
+          array[i+1][j]=1;
+          array[i-1][j]=1;}
+      if(array[i][j+1]&&array[i][j-2]||array[i][j-1]&&array[i][j+2]) //check for wider horizontal gaps
+        { array[i][j]=1;
+          array[i][j+1]=1;
+          array[i][j-1]=1;}
 
     }
   }
 }
-
-void increase_rot(void) //function for testing
+int rot=0; //variable for the humorous rotating triangle page
+void increase_rot(void) //function for increasing rotation
 {
   rot++;
 }
-void convert_to_data(void) //converts the 32x32 bitmap into 4 lines of 32 vertical bytes ready to be printed on the display
+
+void convert_to_data(uint8_t output[128],_Bool array[32][32]) //converts the 32x32 bitmap into 4 lines of 32 vertical bytes ready to be printed on the display
 {
   int i,j,k;
   for(i=0;i<4;i++) //splits data into 4 lines
     for(j=0;j<32;j++) //goes through every single byte
       for(k=8;k>0;k--) //converts single bits of the array into one byte
       {
-        compass_data[j+i*32]=compass_data[j+i*32]<<1; //shift the current value of the byte by 1 (multiply by 2)
-        compass_data[j+i*32]=compass_data[j+i*32]+(1*compass[i*8+k][j]); //add (1*array bit) to the current value
+        output[j+i*32]=output[j+i*32]<<1; //shift the current value of the byte by 1 (multiply by 2)
+        output[j+i*32]=output[j+i*32]+(1*array[i*8+k][j]); //add (1*array bit) to the current value
       }
 }
-void compasswork(void) //function for testing
+void compasswork(void) //function for spinning triangle
 {
-  clear_matrix();
-  rotation((double)rot/10);
+  clear_matrix(compass);
+  rotation((double)rot/10,compass,north);
   increase_rot();
-  fix_matrix();
-  convert_to_data();
+  fix_matrix(compass);
+  convert_to_data(compass_data,compass);
   display_image(96,compass_data);
+}
+
+double seccom=0;
+double mincom=0;
+double hourcom=0;
+void clockwork(void)
+{
+  rotation(0,clock_hour_array,clock_hour); //fix this
+  rotation(0,clock_min_array,clock_min_sec); //fix this
+  clear_matrix(clock_sec_array);
+  seccom+=2*M_PI/60;
+  rotation(seccom,clock_sec_array,clock_min_sec);
+  fix_matrix(clock_sec_array);
+  if(seccom>=2*M_PI)
+  {
+    seccom=2*M_PI/60;
+    clear_matrix(clock_min_array);
+    mincom+=2*M_PI/60;
+    rotation(mincom,clock_min_array,clock_min_sec);
+    fix_matrix(clock_min_array);
+    if(mincom>=2*M_PI)
+    {
+      mincom=2*M_PI/60;
+      clear_matrix(clock_hour_array);
+      hourcom+=2*M_PI/60;
+      rotation(hourcom,clock_hour_array,clock_hour);
+      fix_matrix(clock_hour_array);
+    }
+  }
+  int i,j;
+  for(i=0; i<32; i++)
+    for(j=0; j<32; j++)
+    {
+       clocktotal[i][j]=clock_sec_array[i][j]|clock_min_array[i][j]|clock_hour_array[i][j];
+    }
+
+  convert_to_data(clock_data,clocktotal);
+  display_image(96,clock_data);
 }

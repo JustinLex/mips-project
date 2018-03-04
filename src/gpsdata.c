c7c571027fd978661587085d10dea08790c8bca0
BuildTools <33381901+JustinLex@users.noreply.github.com>
2018-03-04 15:00:31 +0100

    finished implementing UBX-NAV-PVT decoding

--- src/gpsdata.c
+++ src/gpsdata.c
@@ -11,7 +11,7 @@ DEFINEGPSVAR(uint32_t, iTOW) //GPS time of week (milliseconds)
 //macro for pulling data out of payload
 #define EXTRACT(TYPE, VAR, OFFSET) VAR = *((TYPE*)(payload+OFFSET));
 
-/*UBX-NAV-PVT ""*/
+/*UBX-NAV-PVT "Navigation Position, Velocity, and Time Solution"*/
 //unique variables
 DEFINEGPSVAR(uint16_t, year) //current UTC year (years)
 DEFINEGPSVAR(uint8_t, month) //current UTC month (months)
@@ -19,11 +19,21 @@ DEFINEGPSVAR(uint8_t, day) //current UTC day of the month (days)
 DEFINEGPSVAR(uint8_t, hour) //current UTC hour of the day (hours)
 DEFINEGPSVAR(uint8_t, min) //current UTC minute of the hour (minutes)
 DEFINEGPSVAR(uint8_t, sec) //current UTC second of the minute (seconds)
-DEFINEGPSVAR(_Bool, validDate)
-DEFINEGPSVAR(_Bool, validTime)
+DEFINEGPSVAR(_Bool, validDate) //date fields have data (may not be accurate)
+DEFINEGPSVAR(_Bool, validTime) //time fields have data (may not be accurate)
 DEFINEGPSVAR(_Bool, fullyResolved) // true if all UTC time fields are known
 DEFINEGPSVAR(uint32_t, tAcc) //time accuracy estimate (±nanoseconds)
 DEFINEGPSVAR(int32_t, nano) //current UTC nanosecond (nanoseconds)
+DEFINEGPSVAR(uint8_t, fixType) //current status of our GPS Lock
+//(0: no fix, 1: dead reckoning, 2: 2D fix, 3: 3D fix, 4: (not used on ours), 5: time only fix
+DEFINEGPSVAR(uint8_t, numSV) //number of satellites used for data
+DEFINEGPSVAR(int32_t, lon) //longitude (fixed point number, LSB is 1e-7)
+DEFINEGPSVAR(int32_t, lat) //latitude (fixed point number, LSB is 1e-7)
+DEFINEGPSVAR(int32_t, hMSL) //height above sea level (millimeters)
+DEFINEGPSVAR(uint32_t, hAcc) //horizontal accuracy estimate (±millimeters)
+DEFINEGPSVAR(uint32_t, vAcc) //vertical accuracy estimate (±millimeters)
+DEFINEGPSVAR(int32_t, gSpeed) //ground speed (millimeters/second)
+DEFINEGPSVAR(int32_t, headMot) //heading of motion (degrees, LSB is 1E-5)
 
 // payload storer
 void store_nav_pvt_payload(uint8_t* payload) {
@@ -42,6 +52,15 @@ void store_nav_pvt_payload(uint8_t* payload) {
 
   EXTRACT(uint32_t, tAcc, 12)
   EXTRACT(int32_t, nano, 16)
+  EXTRACT(uint8_t, fixType, 20)
+  EXTRACT(uint8_t, numSV, 23)
+  EXTRACT(int32_t, lon, 24)
+  EXTRACT(int32_t, lat, 28)
+  EXTRACT(int32_t, hMSL, 36)
+  EXTRACT(uint32_t, hAcc, 40)
+  EXTRACT(uint32_t, vAcc, 44)
+  EXTRACT(int32_t, gSpeed, 60)
+  EXTRACT(int32_t, headMot, 64)
 }
 
 /*getters*/
@@ -62,3 +81,12 @@ GETTER(_Bool, validTime)
 GETTER(_Bool, fullyResolved)
 GETTER(uint32_t, tAcc)
 GETTER(int32_t, nano)
+GETTER(uint8_t, fixType)
+GETTER(uint8_t, numSV)
+GETTER(int32_t, lon)
+GETTER(int32_t, lat)
+GETTER(int32_t, hMSL)
+GETTER(uint32_t, hAcc)
+GETTER(uint32_t, vAcc)
+GETTER(int32_t, gSpeed)
+GETTER(int32_t, headMot)
